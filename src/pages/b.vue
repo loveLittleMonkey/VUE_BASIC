af3d107721e999e5ad8cb190c36e6041c5689103
梁靖梅 <liangjm@belink.com>
2020-06-02 17:12:23 +0800

    ljm

--- src/pages/b.vue
+++ src/pages/b.vue
@@ -2,15 +2,14 @@
   <div class="home">
     <h2>选择以下服务请点击按钮</h2>
     <div class="btn">
-      <router-link to="/page1">查看人员</router-link>
+      <router-link to="/page1">查看人员 </router-link>
     </div>
     <div class="btn">
-      <router-link to="/add">增加人员</router-link>
+      <router-link to="/add">增加人员 </router-link>
     </div>
     <div class="btn">
       <router-link to="/serve">服务咨询</router-link>
     </div>
-    
   </div>
 </template>
 <script>
@@ -20,15 +19,15 @@ export default {};
 h2 {
   width: 100%;
   height: 200px;
-  line-height:200px;
+  line-height: 200px;
   font-size: 25px;
   text-align: center;
   color: red;
-  margin:0px;
-  background-color:skyblue;
+  margin: 0px;
+  background-color: skyblue;
 }
 .btn {
-  display:flex;
+  display: flex;
   flex-direction: column;
   text-align: left;
   padding: 3px;
@@ -42,6 +41,6 @@ h2 {
   color: #ffffff;
   padding-left: 20px;
   overflow: hidden;
-  background-color:#cccccc;
+  background-color: #cccccc;
 }
 </style>
